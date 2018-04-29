013153d56a0ecc63348aaf0f5578b6fc75666393
ellcode <eyu@upstreamworks.com>
2018-04-29 10:57:11 -0400

    add all files from local to master

---
+++ solenisftp.java
@@ -0,0 +1,84 @@
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+ 
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+ 
+/**
+ * A program demonstrates how to upload files from local computer to a remote
+ * FTP server using Apache Commons Net API.
+ * @author www.codejava.net
+ */
+ 
+ /* running script and result------
+	D:\ACCOUNTS\ELLWIN FILES\working>javac -cp commons-net-3.3.jar solenisft.java
+
+	D:\ACCOUNTS\ELLWIN FILES\working>java -cp commons-net-3.3.jar;. solenisft
+	file #1 has been downloaded successfully
+	file #2 has been downloaded successfully
+ */
+public class solenisftp{
+ 
+    public static void main(String[] args) {
+        String server = "ftp.grants-mailing.ca";
+        int port = 21;
+        String user = "solenis";
+        String pass = "Irm341998";
+ 
+        FTPClient ftpClient = new FTPClient();
+        try {
+ 
+            ftpClient.connect(server, port);
+            ftpClient.login(user, pass);
+            ftpClient.enterLocalPassiveMode();
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+ 
+            // APPROACH #1: using retrieveFile(String, OutputStream)
+            String remoteFile1 = "/fulcanada.solenis.1021150119.pdf";
+            File downloadFile1 = new File("D:/ACCOUNTS/ELLWIN FILES/test/fulcanada.solenis.1021150119.pdf");
+            OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
+            boolean success = ftpClient.retrieveFile(remoteFile1, outputStream1);
+            outputStream1.close();
+ 
+            if (success) {
+                System.out.println("File #1 has been downloaded successfully.");
+            }
+ 
+            // APPROACH #2: using InputStream retrieveFileStream(String)
+            String remoteFile2 = "/fulcanada.solenis.1020150117.pdf";
+            File downloadFile2 = new File("D:/ACCOUNTS/ELLWIN FILES/test/fulcanada.solenis.1020150117.pdf");
+            OutputStream outputStream2 = new BufferedOutputStream(new FileOutputStream(downloadFile2));
+            InputStream inputStream = ftpClient.retrieveFileStream(remoteFile2);
+            byte[] bytesArray = new byte[4096];
+            int bytesRead = -1;
+            while ((bytesRead = inputStream.read(bytesArray)) != -1) {
+                outputStream2.write(bytesArray, 0, bytesRead);
+            }
+ 
+            success = ftpClient.completePendingCommand();
+            if (success) {
+                System.out.println("File #2 has been downloaded successfully.");
+            }
+            outputStream2.close();
+            inputStream.close();
+ 
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (ftpClient.isConnected()) {
+                    ftpClient.logout();
+                    ftpClient.disconnect();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
